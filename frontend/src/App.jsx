32bb4c5a40d046a2c1c80d9a9be809836e055878
Priyanshu885588 <priyanshumandani978@gmail.com>
2024-01-13 14:46:29 +0530

    added tailwind config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <iframe src="https://giphy.com/embed/I8s0tldV0AaR0nnT9i" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/get-er-done-git-r-I8s0tldV0AaR0nnT9i">"lets fucking do this "</a></p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
