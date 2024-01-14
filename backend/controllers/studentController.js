22970acd34f442467b6398fc312641a4500c0e3e
Priyanshu885588 <priyanshumandani978@gmail.com>
2024-01-15 03:52:59 +0530

    added get student batches routes and controllers

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -1,5 +1,21 @@
 const db = require("../db/db");
 
+const getAllBatches = async (req, res) => {
+  try {
+    const [rows] = await db
+      .promise()
+      .query(
+        "SELECT table_name FROM information_schema.tables WHERE table_name LIKE 'student_%'"
+      );
+    const batches = rows.map((row) => row.TABLE_NAME.replace("student_", ""));
+
+    res.status(200).json({ batches });
+  } catch (error) {
+    console.error("Error executing the query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 const getStudentData = async (req, res) => {
   try {
     const { batch } = req.query;
@@ -15,4 +31,5 @@ const getStudentData = async (req, res) => {
 
 module.exports = {
   getStudentData,
+  getAllBatches,
 };
--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const studentController = require("../controllers/studentController");
 
 router.route("/students").get(studentController.getStudentData);
+router.route("/students/batches").get(studentController.getAllBatches);
 
 module.exports = router;
