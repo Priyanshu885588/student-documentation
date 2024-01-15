3fd65ee6578f140cd0bfa8b1e7eb454bce8c59f3
Priyanshu885588 <priyanshumandani978@gmail.com>
2024-01-15 22:57:09 +0530

    added the private route and handled admin login

--- frontend/src/Components/Admin/Navbar/VerticalNavbar.jsx
+++ frontend/src/Components/Admin/Navbar/VerticalNavbar.jsx
@@ -1,49 +1,31 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaChartPie } from "react-icons/fa";
+import { FaUpload } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+import { IoLogOutSharp } from "react-icons/io5";
 export const VerticalNavbar = () => {
   const [isDrawerOpen, setDrawerOpen] = useState(false);
-
+  const navigate = useNavigate();
   const toggleDrawer = () => {
     setDrawerOpen(!isDrawerOpen);
   };
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/admin");
+  };
+
   return (
-    <div>
+    <div className="h-fit">
       <div className="">
         <button
           className="bg-white hover:bg-amber-200 rounded-br-2xl py-2 px-2 absolute top-0 transition-all duration-300"
           type="button"
           onClick={toggleDrawer}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="25"
-            height="25"
-            viewBox="0,0,256,256"
-          >
-            <g
-              fill="#00000"
-              fillRule="nonzero"
-              stroke="none"
-              strokeWidth="1"
-              strokeLinecap="butt"
-              strokeLinejoin="miter"
-              strokeMiterlimit="10"
-              strokeDasharray=""
-              strokeDashoffset="0"
-              fontFamily="none"
-              fontWeight="none"
-              fontSize="none"
-              textAnchor="none"
-              style={{ mixBlendMode: "normal" }}
-            >
-              <g transform="scale(10.66667,10.66667)">
-                <path d="M2,5v2h20v-2zM2,11v2h20v-2zM2,17v2h20v-2z"></path>
-              </g>
-            </g>
-          </svg>
+          <GiHamburgerMenu size="1.5rem" />
         </button>
       </div>
       <div
@@ -64,20 +46,7 @@ export const VerticalNavbar = () => {
           className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
           onClick={toggleDrawer}
         >
-          <svg
-            aria-hidden="true"
-            className="w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <span className="sr-only">Close menu</span>
+          <IoClose size="1.5rem" />
         </button>
         <div className="py-4 overflow-y-auto">
           <ul className="space-y-2 font-medium">
@@ -87,16 +56,7 @@ export const VerticalNavbar = () => {
                 className="flex items-center p-2 text-gray-100 rounded-lg dark:text-white hover:bg-gray-500 dark:hover:bg-gray-700 group"
                 onClick={toggleDrawer}
               >
-                <svg
-                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 21"
-                >
-                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                </svg>
+                <FaChartPie />
                 <span className="ms-3">Dashboard</span>
               </Link>
             </li>
@@ -106,19 +66,18 @@ export const VerticalNavbar = () => {
                 className="flex items-center p-2 text-gray-100 rounded-lg dark:text-white hover:bg-gray-500 dark:hover:bg-gray-700 group"
                 onClick={toggleDrawer}
               >
-                <svg
-                  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
-                  <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
-                  <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
-                </svg>
+                <FaUpload />
                 <span className="ms-3">Upload</span>
               </Link>
+            </li>
+            <li>
+              <button
+                className="flex items-center p-2 text-gray-100 rounded-lg dark:text-white hover:bg-gray-500 dark:hover:bg-gray-700 group w-full"
+                onClick={handleLogout}
+              >
+                <IoLogOutSharp size="1.3rem" />
+                <span className="ms-2">Logout</span>
+              </button>
             </li>
           </ul>
         </div>
