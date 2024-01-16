4a16337ea50acaecef3034d4d6c5d23a8e3f20a5
Priyanshu885588 <priyanshumandani978@gmail.com>
2024-01-17 00:08:10 +0530

    logout function when user leaves the dashboard

--- frontend/src/Components/Admin/AdminAuth/AdminLogin.jsx
+++ frontend/src/Components/Admin/AdminAuth/AdminLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { adminLogin } from "../services/Api"; // Import your API service
 
@@ -25,6 +25,12 @@ export const AdminLogin = () => {
       console.error("Login failed:", error.message);
     }
   };
+  useEffect(() => {
+    const handleLogout = () => {
+      localStorage.removeItem("token");
+    };
+    handleLogout();
+  }, []);
 
   return (
     <div className="flex justify-center items-center h-screen w-full">
