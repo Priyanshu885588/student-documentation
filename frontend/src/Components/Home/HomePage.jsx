02f29b493f0dc2e4b90507cd94eb043b0fbd6a47
24-manish <mastermanish24@gmail.com>
2024-02-07 00:23:52 +0530

    adjusted loginanimation

--- frontend/src/Components/Home/HomePage.jsx
+++ frontend/src/Components/Home/HomePage.jsx
@@ -6,8 +6,17 @@ import { LoginAnimation } from "../UI/loginanimation";
 export const HomePage = () => {
   const [showStartingAnimation, setShowStartingAnimation] = useState(true);
   const [studentlogin, setstudentlogin] = useState(false);
+  const [style, setStyle] = useState("opacity=1");
+  const changeStyle = () => {
+   
+     setStyle("opacity=0");
+};
   const loginbuttonclick = () => {
+   
+    changeStyle();
     setstudentlogin(true);
+    
+
   };
   useEffect(() => {
     const timeoutId = setTimeout(() => {
@@ -21,13 +30,13 @@ export const HomePage = () => {
   ) : (
     <div className="w-screen h-screen flex justify-between items-center flex-col overflow-hidden homepage-bg">
       <div className="flex flex-col justify-center items-center gap-4 mt-7">
-        <h1 className="text-white text-5xl text-center montserrat mb-4 z-10">
+        <h1 className="text-white text-5xl text-center montserrat mb-4 ">
           Student Documents upload portal
         </h1>
-        <h1 className="text-white text-2xl uppercase quicksand z-10">
+        <h1 className="text-white text-2xl uppercase quicksand ">
           RNS Institute of Technology
         </h1>
-        <p className="w-2/3 text-white merriweather italic text-center text-sm opacity-75 font-extralight z-10">
+        <p className="w-2/3 text-white merriweather italic text-center text-sm opacity-75 font-extralight ">
           "to impart quality education to enable graduates to excel in their
           career"
         </p>
@@ -40,8 +49,9 @@ export const HomePage = () => {
       </div>
       <div className="mb-10">
         <button
-          className="text-white px-10 py-2 border-2 backdrop-blur-3xl roboto text-lg rounded-full hover:bg-gray-200 hover:text-black transition-all duration-300"
-          onClick={loginbuttonclick}
+          className="text-white px-10 py-2 border-2 backdrop-blur-3xl roboto text-lg rounded-full hover:bg-gray-200 :text-black transition-all duration-300  "
+          onClick={loginbuttonclick  } 
+          
         >
           Login
         </button>
