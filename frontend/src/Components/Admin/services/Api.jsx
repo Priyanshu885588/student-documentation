61da8acf985a779cbf454a13f96351dd4acbb0a4
Priyanshu885588 <priyanshumandani978@gmail.com>
2024-05-26 02:12:08 +0530

    added new host

--- frontend/src/Components/Admin/services/Api.jsx
+++ frontend/src/Components/Admin/services/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseURL = "http://34.228.8.172/api/v1/admin";
-const baseURL = "https://student-documentation.vercel.app/api/v1/admin";
+const baseURL = "https://student-documentation.onrender.com/api/v1/admin";
 // const baseURL = "http://localhost:3000/api/v1/admin";
 
 const fetchStudentData = async (data) => {
